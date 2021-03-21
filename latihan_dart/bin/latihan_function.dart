022cee9a509557567141d43599fe96ed5b2aa85f
khusaini <@muhammadkhusaini599>
2021-03-21 22:13:37 +0700

    latihan function

---
+++ latihan_dart/bin/latihan_function.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+String say(String from, String message, {String to, String appName}){
+  return from + " say " +  ((to != null ) ? " to " + to : "") +((appName != null) ? " Via " + appName : "");
+}
+
+String say2(String from, String message, {String to, String appName}){
+  return from + " say " +  ((to != null ) ? " to " + to : "") +((appName != null) ? " Via " + appName : "");
+}
+
+main(List<String> args) {
+
+  print(say("Eka"," Hello", to: "Khusaini", appName: "bip")); 
+  
+}
